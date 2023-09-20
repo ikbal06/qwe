ea28edd2eb190d7e979e5c206c7ad21bf2ee4117
Cem Topkaya <cem.topkaya@ulakhaberlesme.com.tr>
2023-09-20 01:38:00 +0000

    virtual env ile calisacak

--- .devcontainer/setup-venv.sh
+++ .devcontainer/setup-venv.sh
@@ -6,8 +6,14 @@
 # .venv adında bir sanal ortam oluştur
 python3 -m venv .venv
 
+# sudo chown -R $USER .venv
+
 # Sanal ortamı etkinleştir
 source .venv/bin/activate
 
 # requirements.txt dosyasındaki paketleri yükle
+# Aşağıdaki komut paketleri "/usr/lib/python3/dist-packages" adresine yükler
 pip install -r requirements.txt
+
+# Aşağıdaki komut paketleri ".venv/lib64/python3.11/site-packages" adresine yükler
+# pip install -t ../.venv/lib64/python3.11/site-packages -r requirements.txt
---
+++ .vscode/scripts/print_vars.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "workspaceFolder: $workspaceFolder"
+echo "aktif dosya adı: $activeFile.name"
+echo "aktif dosya uzantısı: $activeFile.extension"
+echo "aktif dosya yolu: $activeFile.path"
